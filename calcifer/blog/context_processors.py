5086604d92b6614ce811c6529f9da61a7f867ae5
Leonardo Vidarte <lvidarte@gmail.com>
2012-10-08 12:39:09 -0300

    Updated context processors

--- calcifer/blog/context_processors.py
+++ calcifer/blog/context_processors.py
@@ -2,12 +2,9 @@ from django.conf import settings
 
 def blog(request):
     return {
-        'BLOG_NAME': getattr(settings, 'BLOG_NAME', u'calcifer'),
-        'BLOG_DESCRIPTION': getattr(settings,
-                                    'BLOG_DESCRIPTION',
-                                    u'A minimalist blog'),
+        'BLOG_NAME': getattr(settings, 'BLOG_NAME', u''),
+        'BLOG_DESCRIPTION': getattr(settings, 'BLOG_DESCRIPTION', u''),
+        'BLOG_KEYWORDS': getattr(settings, 'BLOG_KEYWORDS', u''),
         'DATE_FORMAT': getattr(settings, 'DATE_FORMAT', 'F j, Y'),
-        'DATETIME_FORMAT': getattr(settings,
-                                   'DATETIME_FORMAT',
-                                   'F j, Y H:i:s'),
+        'DATETIME_FORMAT': getattr(settings, 'DATETIME_FORMAT', 'F j, Y H:i:s'),
     }
