49afe837e0365dec25cfea02adfda973acbfd896
Leonardo Vidarte <lvidarte@gmail.com>
2012-10-19 10:00:28 -0300

    Added feeds url in settings

--- calcifer/blog/context_processors.py
+++ calcifer/blog/context_processors.py
@@ -5,6 +5,7 @@ def blog(request):
         'BLOG_NAME': getattr(settings, 'BLOG_NAME', u''),
         'BLOG_DESCRIPTION': getattr(settings, 'BLOG_DESCRIPTION', u''),
         'BLOG_KEYWORDS': getattr(settings, 'BLOG_KEYWORDS', u''),
+        'BLOG_FEEDS_URL': getattr(settings, 'BLOG_FEEDS_URL', u''),
         'DATE_FORMAT': getattr(settings, 'DATE_FORMAT', 'F j, Y'),
         'DATETIME_FORMAT': getattr(settings, 'DATETIME_FORMAT', 'F j, Y H:i:s'),
         'GOOGLE_ANALYTICS_TEMPLATE': getattr(settings, 'GOOGLE_ANALYTICS_TEMPLATE', u''),
